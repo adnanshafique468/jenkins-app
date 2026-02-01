3034526006b724f39240cafec3607c88bae39f03
adnanshafique468 <askkamboa612@gmail.com>
2026-01-31 23:16:24 -0800

    gone

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,6 +4,12 @@
 
 // export default config;
 
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// }
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
